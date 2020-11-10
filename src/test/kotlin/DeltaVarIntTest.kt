867b58dbb1363475b3c9ed2bbac078b8a78fd17f
Manuel Cerón <ceronman@gmail.com>
2020-11-10 21:28:10 +0100

    Added delta var int.

---
+++ src/test/kotlin/DeltaVarIntTest.kt
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+internal class DeltaVarIntTest {
+    @Test
+    fun encode() {
+        val values = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+        val encoder = DeltaVarInt()
+        val result = encoder.encode(values)
+        assertEquals(values, DeltaVarInt.decode(result))
+    }
+
+    @Test
+    fun encode2() {
+        val values = listOf(10, 20, 30, 60, 90, 200, 2015)
+        val encoder = DeltaVarInt()
+        val result = encoder.encode(values)
+        assertEquals(values, DeltaVarInt.decode(result))
+    }
+
+    @Test
+    fun random() {
+        val encoder = DeltaVarInt()
+        for (i in 0..1000) {
+            val size = Random.nextInt(1000, 10000)
+            val values = ArrayList<Int>(size)
+            var prev = Random.nextInt(0, 20)
+            for (j in 0..size) {
+                val value = prev + Random.nextInt(0, 1000)
+                values.add(value)
+                prev = value
+            }
+            val result = encoder.encode(values)
+            assertEquals(values, DeltaVarInt.decode(result))
+        }
+    }
+}
