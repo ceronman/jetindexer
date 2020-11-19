ceronman/jetindexer

commit 7019107891bd74a31b231856fb5dbeaafc91cd13

Added a small change to allow searching while indexing.

jetindexer/src/main/kotlin/com/ceronman/jetindexer/index.kt
@@ -95,7 +95,7 @@ class InvertedIndex(private val tokenizer: Tokenizer) {
 
         log.info("Adding {} documents in {} chunks of {}", newDocuments.size, chunks.size, chunkSize)
 
-        val jobs = ArrayList<Deferred<List<Shard>>>()
+        val jobs = ArrayList<Job>()
         val progress = AtomicInteger()
         for (chunk in chunks) {
             val job = async(Dispatchers.Default) {
@@ -106,11 +106,7 @@ class InvertedIndex(private val tokenizer: Tokenizer) {
             }
             jobs.add(job)
         }
-        val results = jobs.awaitAll().flatten()
-        results.forEach(Shard::load)
-        rwLock.write {
-            shards.addAll(results)
-        }
+        jobs.joinAll()
         progressCallback?.invoke(100)
     }
 
@@ -214,20 +210,23 @@ class InvertedIndex(private val tokenizer: Tokenizer) {
      * A job that will index a batch of documents. Likely going to be called from multiple coroutines or threads
      * running in parallel.
      */
-    private fun addBatchJob(documents: Collection<Document>, progressCallback: ProgressCallback): List<Shard> {
-        val shards = ArrayList<Shard>()
+    private fun addBatchJob(documents: Collection<Document>, progressCallback: ProgressCallback) {
         val index = ShardWriter(tokenizer)
         for (doc in documents) {
             index.add(doc)
             if (index.overCapacity()) {
-                shards.add(index.writeAndClear())
+                addShard(index.writeAndClear())
             }
             progressCallback?.invoke(doc.id)
         }
         if (index.hasDocuments()) {
-            shards.add(index.writeAndClear())
+            addShard(index.writeAndClear())
         }
-        return shards
+    }
+
+    private fun addShard(shard: Shard) = rwLock.write {
+        shard.load()
+        shards.add(shard)
     }
 }
 

sampleapp/src/main/kotlin/main.kt
@@ -38,7 +38,6 @@ fun main() {
 
     val inputBox = JTextField()
     inputBox.text = "input query"
-    inputBox.isEnabled = false
 
     fun search() {
         GlobalScope.launch(Dispatchers.Default) {
@@ -75,11 +74,9 @@ fun main() {
             )
             GlobalScope.launch(Dispatchers.Default) {
                 progressBar.isVisible = true
+                inputBox.text = ""
                 indexer.index { progress -> progressBar.value = progress }
                 progressBar.isVisible = false
-                inputBox.text = ""
-                inputBox.isEnabled = true
-                inputBox.isEditable = true
                 logArea.text = "^ Type something in the box above!"
                 indexer.watch {
                     search()

commit a9c0b91a4ac77300de57b5e3b59e66783191b448

Fixed bug with certain terms not appearing in search results.
The bug was introduced in 7019107891bd74a31b231856fb5dbeaafc91cd13 when trying
to allow searching while indexing.

The initial batch indexing happens in parallel. Before that change, the indexer
waited until all the batchs were processed and then added the shards to the
index.

With the change to allow searching while indexing, the shards where added by
each worker immediately after the shard is processed. But because there are
multiple jobs creating shards simultaneously, it is possible that the shards
end up added in different document ID order.

Having the shards ordered by documentID is essential for some QueryResolvers to
work correctly. For example, the `TrigramSubstringQueryResolver` was relying on
this to be able to merge multiple search results in linear time. When the
shards got order got messed up, the merging function was producing incorrect
results. Resulting in missng documents from the results.

This fixes the issue by ensuring that every time that a shard is added, the list
of shards is ordered by the first document ID appearing in the shard.

jetindexer/src/main/kotlin/com/ceronman/jetindexer/index.kt
@@ -16,6 +16,7 @@ package com.ceronman.jetindexer
 
 import kotlinx.coroutines.*
 import org.slf4j.LoggerFactory
+import java.lang.IllegalStateException
 import java.nio.ByteBuffer
 import java.nio.file.Files
 import java.nio.file.Path
@@ -122,9 +123,7 @@ class InvertedIndex(private val tokenizer: Tokenizer) {
 
         if (shardWriter.overCapacity()) {
             log.info("ShardWriter is over capacity, writing shard to disk")
-            val shard = shardWriter.writeAndClear()
-            shard.load()
-            shards.add(shard)
+            addShard(shardWriter.writeAndClear())
         }
     }
 
@@ -265,6 +264,7 @@ class InvertedIndex(private val tokenizer: Tokenizer) {
     private fun addShard(shard: Shard) = rwLock.write {
         shard.load()
         shards.add(shard)
+        shards.sortBy { it.firstDocId }
     }
 }
 
@@ -285,11 +285,18 @@ internal class ShardWriter(private val tokenizer: Tokenizer) {
     private val postings = HashMap<String, PostingList>(SHARD_INDEX_CAPACITY)
     private val tokenPositions = HashMap<String, MutableList<Int>>(2048)
     private var size = 0
-
+    private var firstDocId: Int? = null
     /**
      * Adds a new [Document] to the shard
      */
     fun add(document: Document) {
+        if (firstDocId == null) {
+            firstDocId = document.id
+        } else {
+            if (document.id < firstDocId!!) {
+                throw IllegalStateException("A shard can only index incremental files ids")
+            }
+        }
         tokenPositions.clear()
         val tokens = try {
             tokenizer.tokenize(document.path)
@@ -337,6 +344,9 @@ internal class ShardWriter(private val tokenizer: Tokenizer) {
      * Clears the internal state so that new tokens can be indexed.
      */
     fun writeAndClear(): Shard {
+        if (firstDocId == null) {
+            throw IllegalStateException("There are no indexed documents")
+        }
         val shardPath = Files.createTempFile("jetindexer", "shard")
         val tokenOffsets = HashMap<String, IntArray>(postings.size)
         Files.newByteChannel(shardPath, StandardOpenOption.WRITE).use { channel ->
@@ -351,7 +361,7 @@ internal class ShardWriter(private val tokenizer: Tokenizer) {
         }
         postings.clear()
         size = 0
-        return Shard(shardPath, tokenOffsets)
+        return Shard(shardPath, tokenOffsets, firstDocId!!)
     }
 }
 
@@ -374,7 +384,8 @@ internal class ShardWriter(private val tokenizer: Tokenizer) {
  */
 internal class Shard(
     private val path: Path,
-    private val tokenOffsets: Map<String, IntArray>
+    private val tokenOffsets: Map<String, IntArray>,
+    val firstDocId: Int
 ) {
 
     private lateinit var buffer: ByteBuffer

jetindexer/src/main/kotlin/com/ceronman/jetindexer/posting.kt
@@ -145,6 +145,7 @@ class PostingListView(private val buffers: List<ByteBuffer>) {
      */
     fun readPostings(): Sequence<Posting> = sequence {
         for (buffer in buffers) {
+            buffer.rewind()
             while (buffer.hasRemaining()) {
                 val docId = buffer.getVarInt()
                 val numPositions = buffer.getVarInt()